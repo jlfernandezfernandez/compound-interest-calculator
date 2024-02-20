90e13bb1d4bebff5d2a01a214253cd6200ae5e8d
Jordi Luis Fernández Fernánez <jordilff@ext.inditex.com>
2024-02-21 00:05:27 +0100

    adding margin top for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import BuyMeACoffeeButton from "./BuyMeACoffeButton";
 
 export default function Footer() {
     return (
-        <div className="bottom-0 left-0 w-full bg-black bg-opacity-50 text-white text-center py-3 flex justify-between px-6">
+        <div className="mt-3 bottom-0 left-0 w-full bg-black bg-opacity-50 text-white text-center py-3 flex justify-between px-6">
             <div className="flex items-center">
                 <BuyMeACoffeeButton />
             </div>
