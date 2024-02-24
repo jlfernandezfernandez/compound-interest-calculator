76c761a8196ac40494a7610a50faf10c7f2caa41
Jordi Luis Fernández Fernánez <jordi.ff97@gmail.com>
2024-02-24 14:05:35 +0100

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,15 @@ import ArrowRightIcon from './ArrowRightIcon';
 export default function Header() {
     return (
         <header className="border-b p-4 bg-white">
-            <div className="flex items-center space-x-4">
-                <Link className="flex items-center space-x-2 text-lg font-semibold" href="/">
+            <div className="flex items-center space-x-8">
+                <Link className="flex items-center text-lg font-semibold" href="/">
                     <HomeIcon className="h-6 w-6" />
                     Inicio
                 </Link>
                 <h1 className="text-xl font-bold tracking-tighter">Calculadora</h1>
-                <Link className="flex items-center space-x-2 ml-auto text-sm" href="/">
-                    Mejora tus inversiones
-                    <ArrowRightIcon className="h-4 w-4 translate-x-1" />
+                <Link className="flex items-center ml-auto text-sm" href="/">
+                    Mejora tus resultados                    
+                    <ArrowRightIcon className="h-4 w-4s" />
                 </Link>
             </div>
         </header>
