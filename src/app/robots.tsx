bf76b741d64ef8164a6113b277d03f87499ce008
Jordi Luis Fernández Fernánez <jordi.ff97@gmail.com>
2024-02-25 00:31:59 +0100

    fix robots.tsx

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -4,9 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
+      allow: '/'
     },
-    sitemap: 'https://acme.com/sitemap.xml',
+    sitemap: 'https://ganaconinteres.com/sitemap.xml',
   }
 }
