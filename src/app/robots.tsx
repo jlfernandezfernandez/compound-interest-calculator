fd7473a13c18abb218f486707d888ceaa35c349e
Jordi Luis Fernández Fernánez <jordi.ff97@gmail.com>
2024-02-25 11:41:06 +0100

    update sitemap

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/'
     },
-    sitemap: 'https://ganaconinteres.com/sitemap.xml',
+    sitemap: 'https://www.ganaconinteres.com/sitemap.xml',
   }
 }
