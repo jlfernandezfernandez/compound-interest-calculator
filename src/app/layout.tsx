d0ff11a14dc30a60cfc85862a892323ffc2869f1
Jordi Luis Fernández Fernánez <jordi.ff97@gmail.com>
2024-02-26 18:34:39 +0100

    add robots and metadata ts clases

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google"; // Importar Plus Jakarta SNS desde Google Fonts
 import "./globals.css";
 import { Providers } from "@/store/Providers";
@@ -11,13 +11,6 @@ export const metadata: Metadata = {
   title: "¡Calculadora de interés compuesto avanzada en 2024! Optimiza tus inversiones",
   description: "Calcula el interés compuesto de tus productos financieros de manera fácil. Desde fondos de inversión hasta cuentas de ahorro, maximiza tus ganancias con nuestra herramienta avanzada.",
 };
-
-export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-}
 
 export default function RootLayout({
   children,
