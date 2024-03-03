856181397000c794cc617f134bf78f7b23e9067e
Jordi Luis Fernández Fernánez <jordilff@ext.inditex.com>
2024-03-03 23:00:04 +0100

    log buymeacoffe button

--- src/components/BuyMeACoffeButton.tsx
+++ src/components/BuyMeACoffeButton.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export default function BuyMeACoffeeButton() {
+    const handleClick = () => {
+        console.log('Botón "Invítame a un café" pulsado.');
+    };
+
     return (
-            <a href="https://www.buymeacoffee.com/jordiluiss" target="_blank" rel="noopener noreferrer" className="text-sm bg-gray-800 text-white py-2 px-4 rounded-md">
-                Invítame a un café
-                <span role="img" aria-label="café" className="ml-2">☕</span>
-            </a>
+        <a href="https://www.buymeacoffee.com/jordiluiss" target="_blank" rel="noopener noreferrer" 
+           className="text-sm bg-gray-800 text-white py-2 px-4 rounded-md"
+           onClick={handleClick}>
+            Invítame a un café
+            <span role="img" aria-label="café" className="ml-2">☕</span>
+        </a>
     );
 }
