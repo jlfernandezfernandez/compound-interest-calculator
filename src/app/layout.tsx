833ac2cf9330754f374e67e3840294f97a6e2554
Jordi Luis Fernández Fernánez <jordilff@ext.inditex.com>
2024-03-03 15:51:31 +0100

    add viewport theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google"; // Importar Plus Jakarta SNS desde Google Fonts
 import "./globals.css";
 import { Providers } from "@/store/Providers";
@@ -11,6 +11,10 @@ export const metadata: Metadata = {
   title: "Calculadora de Interés Compuesto 2024: Optimiza Inversiones | Herramienta Gratuita",
   description: "Descubre cómo el interés compuesto puede transformar tus ahorros e inversiones en 2024. Utiliza nuestra calculadora gratuita para planificar y optimizar tus estrategias financieras. Adecuada para todo tipo de inversores, desde principiantes hasta avanzados.",
 };
+
+export const viewport: Viewport = {
+  themeColor: 'black',
+}
 
 export default function RootLayout({
   children,
