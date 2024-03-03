cf2e5a02ba8d2940c3b4264b6b91c26af95f0db0
Jordi Luis Fernández Fernánez <jordilff@ext.inditex.com>
2024-03-03 13:21:36 +0100

    add smooth scroll after adding a product

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,18 +1,24 @@
 'use client'
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useAppSelector } from '../store';
 import ProductCard from "./ProductCard";
 
 export default function ProductList() {
-
     const products = useAppSelector(state => state.calculator.products);
+    const lastProductRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (products.length > 0 && lastProductRef.current) {
+            lastProductRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [products]);
 
     return (
         <section className="w-full">
             {products.map((product, index) => (
-                <div key={index} className="mb-4 p-4">
-                    <ProductCard key={product.id} productDetails={product} />
+                <div key={product.id} className="mb-4 p-4" ref={index === products.length - 1 ? lastProductRef : null}>
+                    <ProductCard productDetails={product} />
                 </div>
             ))}
         </section>
