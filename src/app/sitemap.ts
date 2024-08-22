0e47ff7a48e15b158a3ba0085c5fc4f020671378
Jordi Luis Fernández <jordi.ff97@gmail.com>
2024-08-22 22:00:53 +0200

    feature: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,12 +3,6 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.ganaconinteres.com/",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.7,
-    },
-    {
       url: "https://www.ganaconinteres.com/calculadora-interes-compuesto",
       lastModified: new Date(),
       changeFrequency: "monthly",
